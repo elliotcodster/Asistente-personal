3af84d26c7e83da2539564ff7f8be25629511d13
Eliot Martin <124529627+elliotcodster@users.noreply.github.com>
2023-02-05 22:38:27 +0200

    Create Conceptos.js

---
+++ Conceptos.js
@@ -0,0 +1,13 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "@/styles/index.scss";`
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+      }
+    }
+  },
